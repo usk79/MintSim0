9a13c5b71f37dbf15cea96aea616ef42881e968c
usk79 <yusuke.m79@outlook.jp>
2022-07-26 18:53:25 +0900

    simmodel テストの追加 微修正

--- src/simtools/simmodel.rs
+++ src/simtools/simmodel.rs
@@ -45,10 +45,10 @@ pub trait DEModel: Model{
 /// 状態空間モデル
 #[derive(Debug, Clone)]
 pub struct SpaceStateModel {
-    mat_a: DMatrix<f64>,    // 状態遷移行列A
-    mat_b: DMatrix<f64>,    // 入力行列B
-    mat_c: DMatrix<f64>,    // 観測行列C
-    mat_d: DMatrix<f64>,    // 入力行列D
+    mtrx_a: DMatrix<f64>,    // 状態遷移行列A
+    mtrx_b: DMatrix<f64>,    // 入力行列B
+    mtrx_c: DMatrix<f64>,    // 観測行列C
+    mtrx_d: DMatrix<f64>,    // 入力行列D
     state_dim: usize,       // 状態次数
     input_dim: usize,       // 入力次数
     output_dim: usize,      // 出力次数
@@ -59,12 +59,16 @@ pub struct SpaceStateModel {
 }
 
 impl SpaceStateModel {
-    pub fn new(sdim: usize, idim: usize, odim: usize, solvertype: SolverType) -> Self {
-        SpaceStateModel {
-            mat_a: DMatrix::from_element(sdim, sdim, 0.0),
-            mat_b: DMatrix::from_element(sdim, idim, 0.0),
-            mat_c: DMatrix::from_element(odim, sdim, 0.0),
-            mat_d: DMatrix::from_element(odim, idim, 0.0),
+    pub fn new(sdim: usize, idim: usize, odim: usize, solvertype: SolverType) -> anyhow::Result<Self> {
+        if sdim <= 0 || idim <= 0 || odim <= 0 {
+            return Err(anyhow!("状態, 入力, 出力の次数は自然数である必要があります。"));
+        }
+
+        Ok(SpaceStateModel {
+            mtrx_a: DMatrix::from_element(sdim, sdim, 0.0),
+            mtrx_b: DMatrix::from_element(sdim, idim, 0.0),
+            mtrx_c: DMatrix::from_element(odim, sdim, 0.0),
+            mtrx_d: DMatrix::from_element(odim, idim, 0.0),
             x: DMatrix::from_element(sdim, 1, 0.0),
             u: DMatrix::from_element(idim, 1, 0.0),
             state_dim: sdim,
@@ -72,138 +76,11 @@ impl SpaceStateModel {
             output_dim: odim,
             solver: solvertype,
             sigbus: Bus::new(),
-        }
-    }
-
-    // 伝達関数から状態空間モデルを生成する  ⇒　Fromトレイトでの実装に変更すること
-    pub fn from_tf<'a> (num: &'a [f64], den: &'a [f64], solvertype: SolverType) -> anyhow::Result<Self> {
-        let sdim = den.len() - 1;
-        let idim = 1;
-        let odim = 1;
-
-        if sdim < 1 {
-            return Err(anyhow!("次数が0になりました。"))
-        }
-        if num.len() > den.len() {
-            return Err(anyhow!("プロパーな伝達関数ではありません。"))
-        }
-
-        let mut model = SpaceStateModel::new(sdim, idim, odim, solvertype);
-        let an = den[0];
-
-        // A行列の作成
-        let mut mat_a = vec![0.0; sdim * sdim];
-        
-        for r in 0..sdim {
-            for c in 0..sdim {
-                if c == sdim - 1 {
-                    mat_a[r * sdim + c] = -den[sdim - r] / an;
-                }
-                else {
-                    mat_a[r * sdim + c] = 0.0;
-                }
-            }
-
-            if r > 0 {
-                mat_a[r * sdim + r - 1] = 1.0;
-            }
-        }
-
-        model.set_mat_a(&mat_a).context("failed at from_tf()")?;
-
-        // B行列の作成
-        let mut mat_b = vec![0.0; sdim * idim];
-        let bn = 
-            if num.len() - 1 == sdim {
-                num[0]
-            } else {
-                0.0
-            };
-        
-        for r in 0..sdim {
-            if r < num.len() {
-                mat_b[r] = (num[num.len() - r - 1] - den[sdim - r] * bn ) / an;
-            } else {
-                mat_b[r] = (-den[sdim - r] * bn ) / an;
-            }
-
-        }
-        model.set_mat_b(&mat_b).context("failed at from_tf()")?;
-
-        // C行列の作成
-        let mut mat_c = vec![0.0; sdim * odim];
-        mat_c[sdim - 1] = 1.0;
-        model.set_mat_c(&mat_c).context("failed at from_tf()")?;
-
-        // D行列の作成
-        let mat_d = vec![bn; odim * idim];
-        model.set_mat_d(&mat_d).context("failed at from_tf()")?;
-
-        Ok(model)
+        })
     }
 
     pub fn init_state(&mut self, init_state: &[f64]) -> anyhow::Result<()> {
-
-        if init_state.len() != self.state_dim {
-            return Err(anyhow!("状態変数のサイズが違います。"));
-        }
-
-        for (i, elem) in init_state.iter().enumerate() {
-            self.x[i] = *elem;
-        }
-
-        Ok(())
-    }
-
-    pub fn set_mat_a(&mut self, mat_a: &[f64]) -> anyhow::Result<()> {
-
-        if mat_a.len() != self.state_dim * self.state_dim {
-            return Err(anyhow!("A行列のサイズが違います。"));
-        }
-
-        for (i, elem) in mat_a.iter().enumerate() {
-            self.mat_a[(i / self.state_dim, i % self.state_dim)] = *elem;
-        }
-
-        Ok(())
-    }
-
-    pub fn set_mat_b(&mut self, mat_b: &[f64]) -> anyhow::Result<()> {
-        
-        if mat_b.len() != self.state_dim * self.input_dim {
-            return Err(anyhow!("B行列のサイズが違います。"));
-        }
-
-        for (i, elem) in mat_b.iter().enumerate() {
-            self.mat_b[(i / self.input_dim, i % self.input_dim)] = *elem;
-        }
-
-        Ok(())
-    }
-
-    pub fn set_mat_c(&mut self, mat_c: &[f64]) -> anyhow::Result<()> {
-
-        if mat_c.len() != self.output_dim * self.state_dim {
-            return Err(anyhow!("C行列のサイズが違います。"));
-        }
-
-        for (i, elem) in mat_c.iter().enumerate() {
-            self.mat_c[(i / self.state_dim, i % self.state_dim)] = *elem;
-        }
-
-        Ok(())
-    }
-
-    pub fn set_mat_d(&mut self, mat_d: &[f64]) -> anyhow::Result<()> {
-
-        if mat_d.len() != self.output_dim * self.input_dim {
-            return Err(anyhow!("D行列のサイズが違います。"));
-        }
-
-        for (i, elem) in mat_d.iter().enumerate() {
-            self.mat_d[(i / self.input_dim, i % self.input_dim)] = *elem;
-        }
-
+        self.set_x(init_state)?;
         Ok(())
     }
 
@@ -211,9 +88,48 @@ impl SpaceStateModel {
         if x.len() != self.state_dim {
             return Err(anyhow!("状態ベクトルの次数が違います。"))
         }
-        for (i, elem) in x.iter().enumerate() {
-            self.x[i] = *elem;
+        x.iter().enumerate().for_each(|(i, e)| self.x[i] = *e);
+
+        Ok(())
+    }
+
+    pub fn set_mtrx_a(&mut self, mtrx_a: &[f64]) -> anyhow::Result<()> {
+
+        if mtrx_a.len() != self.state_dim * self.state_dim {
+            return Err(anyhow!("A行列のサイズが違います。"));
         }
+        mtrx_a.iter().enumerate().for_each(|(i, e)| self.mtrx_a[(i / self.state_dim, i % self.state_dim)] = *e);
+
+        Ok(())
+    }
+
+    pub fn set_mtrx_b(&mut self, mtrx_b: &[f64]) -> anyhow::Result<()> {
+        
+        if mtrx_b.len() != self.state_dim * self.input_dim {
+            return Err(anyhow!("B行列のサイズが違います。"));
+        }
+
+        mtrx_b.iter().enumerate().for_each(|(i, e)| self.mtrx_b[(i / self.input_dim, i % self.input_dim)] = *e);
+
+        Ok(())
+    }
+
+    pub fn set_mtrx_c(&mut self, mtrx_c: &[f64]) -> anyhow::Result<()> {
+
+        if mtrx_c.len() != self.output_dim * self.state_dim {
+            return Err(anyhow!("C行列のサイズが違います。"));
+        }
+        mtrx_c.iter().enumerate().for_each(|(i, e)| self.mtrx_c[(i / self.state_dim, i % self.state_dim)] = *e);
+
+        Ok(())
+    }
+
+    pub fn set_mtrx_d(&mut self, mtrx_d: &[f64]) -> anyhow::Result<()> {
+
+        if mtrx_d.len() != self.output_dim * self.input_dim {
+            return Err(anyhow!("D行列のサイズが違います。"));
+        }
+        mtrx_d.iter().enumerate().for_each(|(i, e)| self.mtrx_d[(i / self.input_dim, i % self.input_dim)] = *e);
 
         Ok(())
     }
@@ -222,31 +138,93 @@ impl SpaceStateModel {
         if u.len() != self.input_dim {
             return Err(anyhow!("入力ベクトルの次数が違います。"))
         }
-        for (i, elem) in u.iter().enumerate() {
-            self.u[i] = *elem;
-        }
+        u.iter().enumerate().for_each(|(i, e)| self.u[i] = *e);
 
         Ok(())
     }
 
     pub fn get_observation(&self) -> DMatrix<f64> {
-        &self.mat_c * &self.x + &self.mat_d * &self.u
+        &self.mtrx_c * &self.x + &self.mtrx_d * &self.u
     }
 
     pub fn set_sigbus(&mut self, list: Vec<Signal>) {
-        for sig in list.iter() {
-            self.sigbus.push(sig.clone()); // クローンしたけどほかに方法がありそう？
-        }
+        list.into_iter().for_each(|sig| self.sigbus.push(sig));
     }
 
 }
 
+// 伝達関数から状態空間モデルを生成する
+pub fn crate_ssm_from_tf<'a> (num: &'a [f64], den: &'a [f64], solvertype: SolverType) -> anyhow::Result<SpaceStateModel> {
+    let sdim = den.len() - 1;
+    let idim = 1;
+    let odim = 1;
+
+    if sdim < 1 {
+        return Err(anyhow!("状態ベクトルの次数が0になりました。"))
+    }
+    if num.len() > den.len() {
+        return Err(anyhow!("プロパーな伝達関数ではありません。"))
+    }
+
+    let mut model = SpaceStateModel::new(sdim, idim, odim, solvertype)?;
+    let an = den[0];
+
+    // A行列の作成
+    let mut mtrx_a = vec![0.0; sdim * sdim];
+    
+    for r in 0..sdim {
+        for c in 0..sdim {
+            if c == sdim - 1 {
+                mtrx_a[r * sdim + c] = -den[sdim - r] / an;
+            }
+            else {
+                mtrx_a[r * sdim + c] = 0.0;
+            }
+        }
+
+        if r > 0 {
+            mtrx_a[r * sdim + r - 1] = 1.0;
+        }
+    }
+
+    model.set_mtrx_a(&mtrx_a).context("failed at from_tf()")?;
+
+    // B行列の作成
+    let mut mtrx_b = vec![0.0; sdim * idim];
+    let bn = 
+        if num.len() - 1 == sdim {
+            num[0]
+        } else {
+            0.0
+        };
+    
+    for r in 0..sdim {
+        if r < num.len() {
+            mtrx_b[r] = (num[num.len() - r - 1] - den[sdim - r] * bn ) / an;
+        } else {
+            mtrx_b[r] = (-den[sdim - r] * bn ) / an;
+        }
+
+    }
+    model.set_mtrx_b(&mtrx_b).context("failed at from_tf()")?;
+
+    // C行列の作成
+    let mut mtrx_c = vec![0.0; sdim * odim];
+    mtrx_c[sdim - 1] = 1.0;
+    model.set_mtrx_c(&mtrx_c).context("failed at from_tf()")?;
+
+    // D行列の作成
+    let mtrx_d = vec![bn; odim * idim];
+    model.set_mtrx_d(&mtrx_d).context("failed at from_tf()")?;
+
+    Ok(model)
+}
+
+
 impl Model for SpaceStateModel {
     fn output(&mut self) -> &Bus {
         // 結果をバスに保存する
-        for (i, elem) in self.x.iter().enumerate() { 
-            self.sigbus[i].value = *elem;
-        }
+        self.x.iter().enumerate().for_each(|(i, elem)| self.sigbus[i].value = *elem);
 
         &self.sigbus
     }
@@ -261,7 +239,7 @@ impl Model for SpaceStateModel {
 
 impl DEModel for SpaceStateModel {
     fn derivative_func(&self, x: &DMatrix<f64>) -> DMatrix<f64> {
-        &self.mat_a * x + &self.mat_b * &self.u
+        &self.mtrx_a * x + &self.mtrx_b * &self.u
     }
 
     fn set_state(&mut self, newstate: DMatrix<f64>) {
@@ -280,7 +258,7 @@ impl fmt::Display for SpaceStateModel {
         for r in 0..self.state_dim {
             str_a = str_a + &"|";
             for c in 0..self.state_dim {
-                str_a = str_a + &format!("{:>15.5} ", self.mat_a[(r, c)]);
+                str_a = str_a + &format!("{:>15.5} ", self.mtrx_a[(r, c)]);
             }
             str_a = str_a + &format!("|\n")
         }
@@ -289,7 +267,7 @@ impl fmt::Display for SpaceStateModel {
         for r in 0..self.state_dim {
             str_b = str_b + &"|";
             for c in 0..self.input_dim {
-                str_b = str_b + &format!("{:>15.5} ", self.mat_b[(r, c)]);
+                str_b = str_b + &format!("{:>15.5} ", self.mtrx_b[(r, c)]);
             }
             str_b = str_b + &format!("|\n")
         }
@@ -298,7 +276,7 @@ impl fmt::Display for SpaceStateModel {
         for r in 0..self.output_dim {
             str_c = str_c + &"|";
             for c in 0..self.state_dim {
-                str_c = str_c + &format!("{:>15.5} ", self.mat_c[(r, c)]);
+                str_c = str_c + &format!("{:>15.5} ", self.mtrx_c[(r, c)]);
             }
             str_c = str_c + &format!("|\n")
         }
@@ -307,7 +285,7 @@ impl fmt::Display for SpaceStateModel {
         for r in 0..self.output_dim {
             str_d = str_d + &"|";
             for c in 0..self.input_dim {
-                str_d = str_d + &format!("{:>15.5} ", self.mat_d[(r, c)]);
+                str_d = str_d + &format!("{:>15.5} ", self.mtrx_d[(r, c)]);
             }
             str_d = str_d + &format!("|\n")
         }
@@ -327,7 +305,7 @@ pub struct TransFuncModel {
 
 impl TransFuncModel {
     pub fn new(num_coef: &[f64], den_coef: &[f64], solvertype: SolverType) -> anyhow::Result<Self> {
-        let model = SpaceStateModel::from_tf(&num_coef, &den_coef, solvertype).context("Failed to create Trasfer Function Model")?;
+        let model = crate_ssm_from_tf(&num_coef, &den_coef, solvertype).context("Failed to create Trasfer Function Model")?;
         Ok(Self {
             num : num_coef.to_vec(),
             den : den_coef.to_vec(),  
@@ -353,5 +331,132 @@ impl Model for TransFuncModel {
 impl fmt::Display for TransFuncModel {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Transfer Function Model -- >> \n\tnum: {:?}\n\tden: {:?}\n", self.num, self.den)
+    }
+}
+
+
+#[cfg(test)]
+mod simmodel_test {
+    use super::*;
+    // cargo test -- --test-threads=1　シングルスレッドで実行したいとき
+    fn print_typename<T>(_: T) {
+        println!("{}", std::any::type_name::<T>());
+    }
+
+    #[test] // StateSpaceModelのセット時のテスト 
+    fn ssm_settest() {
+        let mut model = SpaceStateModel::new(2, 1, 1, SolverType::Euler).unwrap();
+
+        let mtrx_a = [1.0, 1.0, 1.0, 1.0];
+        model.set_mtrx_a(&mtrx_a);
+        assert_eq!(model.mtrx_a, DMatrix::from_row_slice(2, 2, &mtrx_a));
+
+        let mtrx_b = [1.0, 1.0];
+        model.set_mtrx_b(&mtrx_b);
+        assert_eq!(model.mtrx_b, DMatrix::from_row_slice(2, 1, &mtrx_b));
+
+        let mtrx_c = [1.0, 1.0];
+        model.set_mtrx_c(&mtrx_c);
+        assert_eq!(model.mtrx_c, DMatrix::from_row_slice(1, 2, &mtrx_c));
+
+        let init_state = [1.0, 2.0];
+        model.init_state(&init_state);
+        assert_eq!(model.x, DMatrix::from_row_slice(2, 1, &init_state));
+
+        let x = [1.0, 2.0];
+        model.set_x(&x);
+        assert_eq!(model.x, DMatrix::from_row_slice(2, 1, &init_state));
+
+        let mtrx_d = [1.0];
+        model.set_mtrx_d(&mtrx_d);
+        assert_eq!(model.mtrx_d, DMatrix::from_row_slice(1, 1, &mtrx_d));
+
+        println!("model : {}\n", model);
+    }
+
+    #[test]
+    #[should_panic]
+    fn ssm_set_errtest() {
+        let model = SpaceStateModel::new(0, 0, 0, SolverType::Euler).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn ssm_set_mtrx_a_errtest() {
+        let mut model = SpaceStateModel::new(2, 1, 1, SolverType::Euler).unwrap();
+        model.set_mtrx_a(&[2.0, 1.0]).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn ssm_set_mtrx_b_errtest() {
+        let mut model = SpaceStateModel::new(2, 1, 1, SolverType::Euler).unwrap();
+        model.set_mtrx_b(&[2.0, 1.0, 2.0]).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn ssm_set_mtrx_c_errtest() {
+        let mut model = SpaceStateModel::new(2, 1, 1, SolverType::Euler).unwrap();
+        model.set_mtrx_c(&[2.0, 1.0, 2.0]).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn ssm_set_mtrx_d_errtest() {
+        let mut model = SpaceStateModel::new(2, 1, 1, SolverType::Euler).unwrap();
+        model.set_mtrx_d(&[2.0, 1.0, 2.0]).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn ssm_set_x_errtest() {
+        let mut model = SpaceStateModel::new(2, 1, 1, SolverType::Euler).unwrap();
+        model.set_x(&[2.0, 1.0, 2.0]).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn ssm_initstate_errtest() {
+        let mut model = SpaceStateModel::new(2, 1, 1, SolverType::Euler).unwrap();
+        model.init_state(&[2.0, 1.0, 2.0]).unwrap();
+    }
+
+    
+    #[test]
+    #[should_panic]
+    fn ssm_setu_errtest() {
+        let mut model = SpaceStateModel::new(2, 1, 1, SolverType::Euler).unwrap();
+        model.set_u(&[2.0, 1.0, 2.0]).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn tf_set_errtest1() {
+        let model = TransFuncModel::new(&[1.0, 0.0, 2.0, 2.0], &[2.0, 1.0, 1.0], SolverType::Euler).unwrap();       
+    }
+
+    #[test]
+    #[should_panic]
+    fn tf_set_errtest2() {
+        let model = TransFuncModel::new(&[1.0], &[2.0], SolverType::Euler).unwrap();       
+    }
+
+    #[test]
+    fn tf_settest() {
+        let tfmodel = TransFuncModel::new(&[2.0, 2.0], &[2.0, 1.0, 1.0], SolverType::Euler).unwrap();
+        let mut ssm = SpaceStateModel::new(2, 1, 1, SolverType::Euler).unwrap();
+
+        ssm.set_mtrx_a(&[0.0, -0.5, 1.0, -0.5]);
+        ssm.set_mtrx_b(&[1.0, 1.0]);
+        ssm.set_mtrx_c(&[0.0, 1.0]);
+        ssm.set_mtrx_c(&[0.0]);
+
+        assert_eq!(tfmodel.model.mtrx_a, ssm.mtrx_a);
+        assert_eq!(tfmodel.model.mtrx_b, ssm.mtrx_b);
+        assert_eq!(tfmodel.model.mtrx_c, ssm.mtrx_c);
+        assert_eq!(tfmodel.model.mtrx_d, ssm.mtrx_d);
+
+        println!("{}\n", tfmodel);
     }
 }
